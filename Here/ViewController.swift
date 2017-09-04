1d52a5db80dfab4cee6a81aa02ee774fece41a4c
Ainor Syahrizal <macintorch@gmail.com>
2017-09-05 07:04:11 +0800

    display info to text label

--- Here/ViewController.swift
+++ Here/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     
     var manager = CLLocationManager()
 
+    @IBOutlet weak var latitudeLabel: UILabel!
+    
+    @IBOutlet weak var longitudeLabel: UILabel!
+    
+    @IBOutlet weak var courseLabel: UILabel!
+    
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    @IBOutlet weak var altitudeLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,7 +40,19 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations)
+        
+        let location = locations[0]
+        
+        self.latitudeLabel.text = String(location.coordinate.latitude)
+        
+        self.longitudeLabel.text = String(location.coordinate.longitude)
+        
+        self.courseLabel.text = String(location.course)
+        
+        self.speedLabel.text = String(location.speed)
+        
+        self.altitudeLabel.text = String(location.altitude)
+
     }
 
     override func didReceiveMemoryWarning() {
